65ba58089f89026e9ee1fcefcc757b070bd85d35
barbara-alves-atos <104013406+barbara-alves-atos@users.noreply.github.com>
2022-12-12 15:09:23 -0300

    criação de app com react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import logo from './img/abi-logo.png';
+import atos from './img/atos_logo_bottom-removebg-preview.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="direct-chat-messages"></div>
+      <form class="ui form">
+        <textarea placeholder="Digite aqui..." rows="10">
+
+        </textarea>
+        <button class="button" type="submit">Enviar</button>
+      </form>
+      <img src={atos} className="logo-atos" alt="logo atos" />
     </div>
   );
 }
